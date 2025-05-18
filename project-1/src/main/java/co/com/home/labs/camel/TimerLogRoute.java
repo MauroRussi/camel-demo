4e77c2fab95ce2d01ba0f587f080dce5ea9a5498
mrussi <mrussi@theworkshop.com>
2025-05-18 19:35:05 +0200

    Removed target folder and log message

--- project-1/src/main/java/co/com/home/labs/camel/TimerLogRoute.java
+++ project-1/src/main/java/co/com/home/labs/camel/TimerLogRoute.java
@@ -6,7 +6,7 @@ import org.apache.camel.builder.RouteBuilder;
 public class TimerLogRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
-        from("timer:example?period=2000")
+        from("timer:project-1?period=2000")
                 .setBody(constant("Hello Quarkus and Camel"))
                 .to("log:" + TimerLogRoute.class.getName() );
     }
