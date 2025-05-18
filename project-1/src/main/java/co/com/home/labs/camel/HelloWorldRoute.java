ea9d9f2554feffc49214dcc40d5fe74a102a79b8
mrussi <mrussi@theworkshop.com>
2025-05-18 15:44:06 +0200

    Added timer route sample

---
+++ project-1/src/main/java/co/com/home/labs/camel/HelloWorldRoute.java
@@ -0,0 +1,13 @@
+package co.com.home.labs.camel;
+
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class HelloWorldRoute extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+        from("timer:example?period=2000")
+                .setBody(constant("Hello World"))
+                .to("log:" + HelloWorldRoute.class.getName() );
+    }
+}
